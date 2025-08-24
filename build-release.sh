3d574c2ddaccd7348c5f7c07d27600c9d104fd1c
vozodo <contact@vozodo.it>
2025-08-24 23:14:00 +0200

    feat: building tool

---
+++ build-release.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# get version number from manifest
+version=$(jq -r '.version' manifest.json)
+
+base_name="DynSign"
+zip_name="${base_name}-v${version}.zip"
+xpi_name="${base_name}-v${version}.xpi"
+temp_dir="temp_zip"
+
+mkdir -p "$temp_dir"
+
+rsync -av --progress \
+  --exclude='.git/' \
+  --exclude='docs/' \
+  --exclude-from='.gitignore' \
+  --exclude='create-xpi.sh' \
+  --exclude='.gitignore' \
+  --exclude='.gitkeep' \
+  . "$temp_dir" --exclude="$temp_dir"
+
+cd "$temp_dir" || exit 1
+zip -r "../$zip_name" . > /dev/null
+cd ..
+
+cp "$zip_name" "$xpi_name"
+rm -rf "$temp_dir"
+
+echo 
+echo
+echo "XPI-File created: $xpi_name"
+echo "ZIP-File created: $zip_name"
+
+echo
+echo "Content of $zip_name"
+unzip -l "$zip_name"
